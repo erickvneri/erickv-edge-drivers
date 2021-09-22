b15aabc4fefd82ca26d2a2548ec3f70d6e1084fb
erickv <erickv@bluetrailsoft.com>
2021-09-22 13:56:26 -0500

    PowerMeter support

--- src/controller/init.lua
+++ src/controller/init.lua
@@ -1,11 +1,12 @@
 local clusters = require 'st.zigbee.zcl.clusters'
 local OnOff = clusters.OnOff
-local SimpleMetering = clusters.SimpleMetering
-local PowerConfiguration = clusters.PowerConfiguration
+local ElectricalMeasurement = clusters.ElectricalMeasurement
 
-local data_types = require 'st.zigbee.data_types'
 local caps = require 'st.capabilities'
 local log = require 'log'
+--
+-- SubDriver attribute configurations
+local attr_config = require 'controller.attr_config'
 
 -- Pretty printer
 local pprint = function(msg)
@@ -24,14 +25,14 @@ local SUPPORTED_FINGERPRINTS = {
 ------------------------------
 ---- OAuth-like interface ----
 local function is_supported (opts, driver, device)
-  --log.info('>> DRIVER ASSIGNATION')
-  --log.info('>> DEVICE MANUFACTURER:  '..device:get_manufacturer())
-  --log.info('>> DEVICE MODEL:         '..device:get_model())
+  local mn = device:get_manufacturer()
+  local model = device:get_model()
+  log.info('>> [VALIDATE FINGERPRINT] manufacturer: '..mn..', model: '..model)
 
   for _, fingerprint in ipairs(SUPPORTED_FINGERPRINTS) do
-    if device:get_manufacturer() == fingerprint.mn then
-      if device:get_model() == fingerprint.model then
-        --log.info('>> [SUCCESS] FINGERPRINT SUPPORTED')
+    if mn == fingerprint.mn then
+      if model == fingerprint.model then
+        log.info('>> [SUCCESS] FINGERPRINT SUPPORTED')
         return true
       end
     end
@@ -42,13 +43,12 @@ end
 
 
 --------------------------------------
----- Device Capability Controlers ----
+---- Device Capability Controllers ----
 local function onoff_handler(driver, device, command)
-  log.debug('>> ONOFF_HANDLER CALLED')
+  log.debug('>> [APP_REPORT] OnOff ZigbeeMessageTx sent')
 
   local endpoint = device:get_endpoint_for_component_id(command.component)
 
-  pprint(command)
   if command.command == 'on' then
     -- Platform command
     device:emit_event_for_endpoint(endpoint, caps.switch.switch.on())
@@ -62,8 +62,8 @@ local function onoff_handler(driver, device, command)
   end
 end
 
-local function handle_onoff_update(_, device, command, zb_rx)
-  log.debug('>> [OnOff] ZigbeeMessageRx')
+local function handle_onoff_remote(_, device, command, zb_rx)
+  log.debug('>> [EXTERNAL_REPORT] OnOff ZigbeeMessageRx received')
   local endpoint = zb_rx.address_header.src_endpoint.value
   local onoff = command.value and caps.switch.switch.on() or caps.switch.switch.off()
 
@@ -71,29 +71,33 @@ local function handle_onoff_update(_, device, command, zb_rx)
   device:emit_event_for_endpoint(endpoint, onoff)
 end
 
+local function handle_power_remote(_, device, command, zb_rx)
+  log.info('>> [EXTERNAL_REPORT] ElectricalMeasurement ZigbeeMessageRx received')
+  --local endpoint = zb_rx.address_header.src_endpoint.value
+  local endpoint = device:get_endpoint_for_component_id('power')
 
----------------------------------------------
----- Controller SubDriver & Configuration----
-local onoff_cluster_config = {
-  cluster=OnOff.ID,
-  attribute=OnOff.attributes.OnOff.ID,
-  minimum_interval=0,
-  maximum_interval=300,
-  data_type=data_types.Boolean,
-  monitored=true
-}
+  -- Platform event
+  device:emit_event_for_endpoint(endpoint, caps.powerMeter.power(command.value / 10))
+end
 
+------------------------------
+---- Controller SubDriver ----
 local controller_subdriver = {
   NAME = 'controller',
   cluster_configuration = {
-    [caps.switch.ID] = onoff_cluster_config
+    [caps.switch.ID] = attr_config.onoff_cluster_config,
+    [caps.powerMeter.ID] = attr_config.power_cluster_config
   },
   can_handle = is_supported,
   zigbee_handlers = {
     attr = {
       -- Switch
       [OnOff.ID] = {
-        [OnOff.attributes.OnOff.ID] = handle_onoff_update
+        [OnOff.attributes.OnOff.ID] = handle_onoff_remote
+      },
+      -- Power Meter
+      [ElectricalMeasurement.ID] = {
+        [ElectricalMeasurement.attributes.ActivePower.ID] = handle_power_remote
       }
     }
   },
