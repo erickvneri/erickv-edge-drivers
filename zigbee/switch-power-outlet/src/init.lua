96f3b671a3c26968d21e82e75b5cb35d3e14d601
erickv <erickv@bluetrailsoft.com>
2021-12-10 13:54:44 -0600

    patched definitions

--- zigbee/switch-power-outlet/src/init.lua
+++ zigbee/switch-power-outlet/src/init.lua
@@ -1,15 +1,10 @@
-local log = require 'log'
 local caps = require 'st.capabilities'
 local ZigbeeDriver = require 'st.zigbee'
-local zcl_clusters = 'st.zigbee.zcl.clusters'
-
--- attribute clusters
-local OnOff = zcl_clusters.OnOff
-local SimpleMetering = zcl_clusters.SimpleMetering
-
+--
 -- lifecycle_handlers
 local do_configure = require 'lifecycles'.do_configure
 local device_init = require 'lifecycles'.device_init
+
 
 --------------------------- Driver Setup ---------------------------
 local zigbee_switch_power_template = {
@@ -21,10 +16,11 @@ local zigbee_switch_power_template = {
     init = device_init,
     doConfigure = do_configure
   },
-  sub_drivers = { require('controller') },
+  sub_drivers = { require('switch-controller') },
 }
 
 local zigbee_switch_power_driver =
   ZigbeeDriver("samjin_switch_power", zigbee_switch_power_template)
+
 -------------------------- Driver Execute --------------------------
 zigbee_switch_power_driver:run()
