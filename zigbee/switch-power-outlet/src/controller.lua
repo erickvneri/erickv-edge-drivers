0cfb7d5bed6615709e528c32d0f9e0332c78b959
erickv <erickv@bluetrailsoft.com>
2021-12-10 20:57:55 -0600

    removed subdriver, controller at root, improved onoff

---
+++ zigbee/switch-power-outlet/src/controller.lua
@@ -0,0 +1,49 @@
+local caps = require 'st.capabilities'
+local clusters = require 'st.zigbee.zcl.clusters'
+local OnOff = clusters.OnOff
+local log = require 'log'
+
+local controller = {}
+
+-- [[
+-- Handles Switch command triggered from
+-- the SmartThings Platform (app, scene, rule,
+-- SmartApp, etc)
+-- ]]
+function controller.onoff_handler(_, device, command)
+  log.debug('>> [APP_REPORT] OnOff ZigbeeMessageTx sent')
+  local ep = device:get_endpoint_for_component_id(command.component)
+  local attr = OnOff.server.commands
+
+  -- Define command
+  local onoff = command.command == 'on' and attr.On or attr.Off
+  return device:send(onoff(device):to_endpoint(ep))
+end
+
+-- [[
+-- Handles ZigbeeMessageRx based on
+-- configured reporting for OnOff.OnOff
+-- ]]
+function controller.handle_onoff_remote(_, device, command, zb_rx)
+  log.debug('>> [EXTERNAL_REPORT] OnOff ZigbeeMessageRx received')
+  local endpoint = zb_rx.address_header.src_endpoint.value
+  local onoff = command.value and caps.switch.switch.on() or caps.switch.switch.off()
+
+  -- Platform event
+  return device:emit_event_for_endpoint(endpoint, onoff)
+end
+
+-- [[
+-- Handles ZigbeeMessageRx based on
+-- configured reporting for ElectricalMeasurement.ActivePower
+-- ]]
+function controller.handle_power_remote(_, device, command, zb_rx)
+  log.info('>> [EXTERNAL_REPORT] ElectricalMeasurement ZigbeeMessageRx received')
+  local endpoint = zb_rx.address_header.src_endpoint.value
+
+  return device:emit_event_for_endpoint(
+    endpoint, caps.powerMeter.power(command.value / 10))
+end
+
+
+return controller
--- zigbee/switch-power-outlet/src/init.lua
+++ zigbee/switch-power-outlet/src/init.lua
@@ -1,13 +1,16 @@
 local caps = require 'st.capabilities'
 local ZigbeeDriver = require 'st.zigbee'
---
--- lifecycle_handlers
+local clusters = require 'st.zigbee.zcl.clusters'
+local OnOff = clusters.OnOff
+local ElectricalMeasurement = clusters.ElectricalMeasurement
+
+-- local modules
 local do_configure = require 'lifecycles'.do_configure
 local device_init = require 'lifecycles'.device_init
-
+local controller = require 'controller'
 
 --------------------------- Driver Setup ---------------------------
-local zigbee_switch_power_template = {
+local driver_config = {
   supported_capabilities = {
     caps.switch,
     caps.powerMeter
@@ -16,11 +19,32 @@ local zigbee_switch_power_template = {
     init = device_init,
     doConfigure = do_configure
   },
-  sub_drivers = { require('controller') },
+  zigbee_handlers = {
+    attr = {
+      -- Switch
+      [OnOff.ID] = {
+        [OnOff.attributes.OnOff.ID] = controller.handle_onoff_remote
+      },
+      -- Power Meter
+      [ElectricalMeasurement.ID] = {
+        [ElectricalMeasurement.attributes.ActivePower.ID] = controller.handle_power_remote
+      }
+    }
+  },
+  capability_handlers = {
+    [caps.switch.ID] = {
+      [caps.switch.commands.on.NAME] = controller.onoff_handler,
+      [caps.switch.commands.off.NAME] = controller.onoff_handler
+    },
+    [caps.refresh.ID] = {
+      [caps.refresh.commands.refresh.NAME] = function(_, device)
+        device:refresh()
+      end
+    }
+  }
 }
 
-local zigbee_switch_power_driver =
-  ZigbeeDriver("switch_power_outlet", zigbee_switch_power_template)
+local driver = ZigbeeDriver("switch_power_outlet", driver_config)
 
 -------------------------- Driver Execute --------------------------
-zigbee_switch_power_driver:run()
+driver:run()
