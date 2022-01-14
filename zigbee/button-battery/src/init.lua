bdf25ac9dfe34b0bfa5c5f3c6f0f9074c927f398
erickv <erickv@bluetrailsoft.com>
2022-01-13 23:13:56 -0600

    imports, cluster configs, custom onoff attr

--- zigbee/button-battery/src/init.lua
+++ zigbee/button-battery/src/init.lua
@@ -1,43 +1,37 @@
--- MIT License
+-- Copyright 2022 Erick Israel Vazquez Neri
 --
--- Copyright (c) 2021 Erick Israel Vazquez Neri
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
 --
--- Permission is hereby granted, free of charge, to any person obtaining a copy
--- of this software and associated documentation files (the "Software"), to deal
--- in the Software without restriction, including without limitation the rights
--- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
--- copies of the Software, and to permit persons to whom the Software is
--- furnished to do so, subject to the following conditions:
+--     http://www.apache.org/licenses/LICENSE-2.0
 --
--- The above copyright notice and this permission notice shall be included in all
--- copies or substantial portions of the Software.
---
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
--- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
--- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
--- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
--- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
--- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
--- SOFTWARE.
-local caps = require 'st.capabilities'
-local ZigbeeDriver = require 'st.zigbee'
-local clusters = require 'st.zigbee.zcl.clusters'
-local PowerConfiguration = clusters.PowerConfiguration
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+local Battery = require "st.capabilities".battery
+local Button = require "st.capabilities".button
+
+local ZigbeeDriver = require "st.zigbee"
+local PowerConfiguration = require "st.zigbee.zcl.clusters".PowerConfiguration
+local OnOff = require "st.zigbee.zcl.clusters".OnOff
+local OnOffButtonCommandId = 0xFD
 
 -- local modules
-local init = require 'lifecycles'.init
-local added = require 'lifecycles'.added
-local do_configure = require 'lifecycles'.do_configure
---local cluster_handler = require 'event_handlers'.cluster_handler
---local event_emitter = require 'event_handlers'.emitter
+local init = require "lifecycles".init
+local added = require "lifecycles".added
+local do_configure = require "lifecycles".do_configure
+
+local emitter = require "event_handlers".emitter
 
 
 -- Edge Driver Configuration
 local driver_config = {
   supported_capabilities = {
-    caps.button,
-    caps.refresh,
-    caps.battery
+    Battery,
+    Button
   },
   lifecycle_handlers = {
     init = init,
@@ -47,11 +41,16 @@ local driver_config = {
   zigbee_handlers = {
     attr = {
       [PowerConfiguration.ID] = {
-        [PowerConfiguration.attributes.BatteryPercentageRemaining.ID] = nil
+        [PowerConfiguration.attributes.BatteryPercentageRemaining.ID] = emitter.send_battery_level_event
+      }
+    },
+    cluster = {
+      [OnOff.ID] = {
+        [0xFD] = emitter.send_button_event
       }
     }
   }
 }
 
-local driver = ZigbeeDriver('button-battery', driver_config)
+local driver = ZigbeeDriver("button-battery", driver_config)
 driver:run()
