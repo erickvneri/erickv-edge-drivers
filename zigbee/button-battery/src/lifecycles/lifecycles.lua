34b226e9b76aa9c0dd4d0e415a94ebfc7c1dd92a
erickv <erickv@bluetrailsoft.com>
2022-01-13 23:14:13 -0600

    cleaned up lifecycle handlers

--- zigbee/button-battery/src/lifecycles/lifecycles.lua
+++ zigbee/button-battery/src/lifecycles/lifecycles.lua
@@ -1,34 +1,45 @@
--- MIT License
+-- Copyright 2022 Erick Israel Vazquez Neri
 --
--- Copyright (c) 2021 Erick Israel Vazquez Neri
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
 --
--- Permission is hereby granted, free of charge, to any person obtaining a copy
--- of this software and associated documentation files (the "Software"), to deal
--- in the Software without restriction, including without limitation the rights
--- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
--- copies of the Software, and to permit persons to whom the Software is
--- furnished to do so, subject to the following conditions:
+--     http://www.apache.org/licenses/LICENSE-2.0
 --
--- The above copyright notice and this permission notice shall be included in all
--- copies or substantial portions of the Software.
---
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
--- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
--- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
--- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
--- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
--- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
--- SOFTWARE.
-local caps = require 'st.capabilities'
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+local Battery = require "st.capabilities".battery
+local Button = require "st.capabilities".button
+
+local PowerConfiguration = require "st.zigbee.zcl.clusters".PowerConfiguration
+local OnOff = require "st.zigbee.zcl.clusters".OnOff
+local OnOffButton = require "custom".OnOffButton
+local ReadTuyaCluster = require "custom".ReadTuyaCluster
+
+local send_cluster_bind_request = require "event_handlers.emitter".send_cluster_bind_request
+local send_attr_configure_reporting = require "event_handlers.emitter".send_attr_configure_reporting
 
 
-local function _component_to_endpoint(device, component_id)
+-- generates endpoint reference based
+-- on component id string reference.
+--
+-- @param component_id string
+local function _component_to_endpoint(_, component_id)
     local ep = component_id:match("button(%d)")
     return ep and tonumber(ep) or 1
 end
 
+
+-- generates component id reference
+-- based on endpoint notified by
+-- _component_to_endpoint.
+--
+-- @param ep number (endpoint)
 local function _endpoint_to_component(_, ep)
-  return tonumber(ep) == 1 and 'main' or 'button'..ep
+  return tonumber(ep) == 1 and "main" or "button"..ep
 end
 
 
@@ -36,6 +47,8 @@ end
 --
 -- handler component vs endpoint
 -- configuration for consistency
+--
+-- @param device ZigbeeDevice
 local function init(_, device)
   device:set_component_to_endpoint_fn(_component_to_endpoint)
   device:set_endpoint_to_component_fn(_endpoint_to_component)
@@ -46,12 +59,20 @@ end
 --
 -- handles initual
 -- state configuration
-local function added(driver, device)
+--
+-- @param device ZigbeeDevice
+local function added(_, device)
   local number_of_buttons = device:component_count()
+
+  -- define number of buttons by
+  -- components supported by profile
+  device:emit_event(Button.numberOfButtons({ value=number_of_buttons }))
+
+  -- for each component, configure
+  -- supported button events
   for ep=1, number_of_buttons do
-    device:emit_event(caps.button.numberOfButtons({ value=number_of_buttons }))
     device:emit_event_for_endpoint(
-      ep, caps.button.supportedButtonValues({ "pushed", "double", "held" }))
+      ep, Button.supportedButtonValues({ "pushed", "double", "held" }))
   end
 end
 
@@ -61,8 +82,44 @@ end
 -- handles cluster attribute
 -- configuration of report and
 -- request binding
-local function do_configure(driver, added)
+--
+-- @param driver ZigbeeDriver
+-- @param device ZigbeeDevice
+local function do_configure(driver, device)
+  local err = "failed to configure reporting: "
+  local hub_zigbee_eui = driver.environment_info.hub_zigbee_eui
+  -- [[
+  -- Battery capability setup
+  -- ]]
+  assert(device:supports_capability_by_id(Battery.ID), "<Battery> capability not supported")
+  assert(send_cluster_bind_request(
+    device, hub_zigbee_eui, PowerConfiguration.ID))
+  assert(send_attr_configure_reporting(
+    device, PowerConfiguration.attributes.BatteryPercentageRemaining),
+    err.."PowerConfiguration.BatteryPercentageRemaining")
+  assert(send_attr_configure_reporting(
+    device, PowerConfiguration.attributes.BatteryVoltage),
+    err.."PowerConfiguration.BatteryVoltage")
 
+  --[[
+  -- Button capability setup
+  --]]
+  assert(device:supports_capability_by_id(Button.ID), "<Button> capability not supported")
+  assert(send_cluster_bind_request(device, hub_zigbee_eui, OnOff.ID))
+  -- TODO: CHECK PURPOSE OF DeviceTemperatureConfiguration CLUSTER
+  -- TODO: CHECK PURPOSE OF Identify.IdentifyTime CLUSTER
+  -- TODO: CHECK PURPOSE OF Groups CLUSTER
+  -- Send ZigbeeMesssageTx
+  assert(pcall(device.send, device, ReadTuyaCluster(device)))
+  assert(pcall(device.send, device, OnOffButton:read(device)))
+  assert(pcall(device.send, device ,PowerConfiguration.attributes.BatteryPercentageRemaining:read(device)))
+  assert(pcall(device.send, device, PowerConfiguration.attributes.BatteryVoltage:read(device)))
+  assert(pcall(device.send, device, OnOffButton:write(device, 0x30)))
+  assert(pcall(device.send, device, OnOffButton:read(device)))
+
+  -- Submit configuration
+  assert(pcall(device.configure, device))
+  assert(pcall(device.refresh, device))
 end
 
 
