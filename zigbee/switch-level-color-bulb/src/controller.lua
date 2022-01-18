230d325907278c5c90ee5a8b54ba2a79f2c813ef
erickv <erickv@bluetrailsoft.com>
2022-01-18 06:50:41 -0600

    license update

--- zigbee/switch-level-color-bulb/src/controller.lua
+++ zigbee/switch-level-color-bulb/src/controller.lua
@@ -1,3 +1,16 @@
+-- Copyright 2022 Erick Israel Vazquez Neri
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
 local caps = require 'st.capabilities'
 local clusters = require 'st.zigbee.zcl.clusters'
 local OnOff = clusters.OnOff
@@ -18,7 +31,7 @@ local function _send_move_to_level(device, endpoint, level, transition, ...)
   --   options_mask:      LevelOptions  0x00
   --   options_override:  LevelOptions  0x00
   --]]
-  local lvl = math.floor(((level * 0xFF) / 0x64) + 0,5)
+  local lvl = math.floor(((level * 0xFF) / 0x64) + 0.5)
 
   return pcall(
     device.send,
