c122fe9df9d75021c4a4d8952b1b0ec002fded28
erickv <erickv@bluetrailsoft.com>
2022-01-22 13:44:27 -0600

    added abstraction of unknown Basic attr

--- zigbee/button-battery/src/custom/UnknownBasic.lua
+++ zigbee/button-battery/src/custom/UnknownBasic.lua
@@ -11,8 +11,19 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
-return {
-  OnOffButton = require "custom.OnOffButton",
-  ReadTuyaSpecific = require "custom.ReadTuyaSpecific",
-  UnknownBasic = require "custom.UnknownBasic"
-}
+local build_cluster_attribute = require "st.zigbee.cluster_base".build_cluster_attribute
+local Basic = require "st.zigbee.zcl.clusters".Basic
+local Enum8 = require "st.zigbee.data_types".Enum8
+local UnknownBasicAttrId = 0xFFFE
+local UnknownBasicAttrLabel = "UnknownBasicTuyaAttr"
+
+
+-- returns prebuilt attribute
+-- on top of Basic (0x0000) cluster
+return assert(
+  build_cluster_attribute(
+  Basic,
+  UnknownBasicAttrId,
+  UnknownBasicAttrLabel,
+  Enum8,
+  false))
